06d137f64600ebe9cb282d63189f79d42c5e576d
Grant Barchard <grant.barchard@gmail.com>
2025-12-15 23:46:13 -0500

    local dev ts checker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import flowbiteReact from "flowbite-react/plugin/vite";
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import flowbiteReact from "flowbite-react/plugin/vite"
+import checker from "vite-plugin-checker"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
-});
+  plugins: [
+    react(),
+    tailwindcss(),
+    flowbiteReact(),
+    checker({ typescript: { tsconfigPath: "./tsconfig.app.json" } }),
+  ],
+})
