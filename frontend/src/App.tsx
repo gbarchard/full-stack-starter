bfa3eabc29e12733876fefc46b93b1ab733b893f
Grant Barchard <grant.barchard@gmail.com>
2025-12-17 17:08:09 -0500

    client server communication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,15 @@
 import { Button } from "flowbite-react"
+import { useCallback, useEffect } from "react"
 
 export default function App() {
+  const request = useCallback(async () => {
+    const res = await fetch("http://localhost:3000")
+    const text = await res.text()
+    console.log(text)
+  }, [])
+  useEffect(() => {
+    request()
+  }, [request])
+
   return <Button color="default">Yo</Button>
 }
