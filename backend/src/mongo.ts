951966e461d5f2a964ab873fe831db45146797d9
Grant Barchard <grant.barchard@gmail.com>
2026-04-28 14:05:38 -0400

    prettier on server

--- backend/src/mongo.ts
+++ backend/src/mongo.ts
@@ -1,21 +1,21 @@
-import { Db, MongoClient } from "mongodb";
+import { Db, MongoClient } from "mongodb"
 
-const url = "mongodb://host.docker.internal:27017";
-const client = new MongoClient(url);
+const url = "mongodb://host.docker.internal:27017"
+const client = new MongoClient(url)
 
-const dbName = "full_stack_starter";
+const dbName = "full_stack_starter"
 
-let mongodb: Db;
+let mongodb: Db
 
 async function main() {
-  console.log("conntecting...");
-  await client.connect();
-  console.log("Connected successfully to server");
-  mongodb = client.db(dbName);
+  console.log("conntecting...")
+  await client.connect()
+  console.log("Connected successfully to server")
+  mongodb = client.db(dbName)
 
-  return "done.";
+  return "done."
 }
 
-const connectDb = () => main().then(console.log).catch(console.error);
+const connectDb = () => main().then(console.log).catch(console.error)
 
-export { mongodb, connectDb };
+export { mongodb, connectDb }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import cors from "cors";
-import { mongodb, connectDb } from "./mongo.ts";
+import express from "express"
+import cors from "cors"
+import { mongodb, connectDb } from "./mongo.ts"
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(cors());
+app.use(cors())
 
 app.get("/", async (req, res) => {
   // const collections = await mongodb.collections();
@@ -14,10 +14,10 @@ app.get("/", async (req, res) => {
   //   collections.map((c) => c.collectionName),
   // );
   // await mongodb.collection("test").insertOne({ hello: "world" });
-  return res.send();
-});
+  return res.send()
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-  connectDb();
-});
+  console.log(`Example app listening on port ${port}`)
+  connectDb()
+})
