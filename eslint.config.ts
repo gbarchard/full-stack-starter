aa75a64472bb58a57c51365f1b2206544eeb67f4
Grant Barchard <grant.barchard@gmail.com>
2026-04-29 10:39:38 -0400

    root level eslint

---
+++ eslint.config.ts
@@ -0,0 +1,18 @@
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default defineConfig([
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    plugins: {
+      // @ts-expect-error this works, but react hooks is being difficult
+      "react-hooks": reactHooks,
+    },
+    rules: reactHooks.configs.recommended.rules,
+  },
+  reactRefresh.configs.recommended,
+]);
